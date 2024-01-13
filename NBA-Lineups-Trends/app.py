27e947861ad4f0e5a66ae0e7bae15a92483f1d30
Sravan Pannala <57152030+sravanpannala@users.noreply.github.com>
2024-01-13 14:44:54 -0500

    lineups fix typo in credits

--- NBA-Lineups-Trends/app.py
+++ NBA-Lineups-Trends/app.py
@@ -285,7 +285,7 @@ def server(input, output, session):
                         + "Total Possessions Played: " + str(totals["Poss"]) + " | " 
                         + "Total Minutes Played: " + str(totals["Minutes"]) 
                     ),
-                    caption="@SravanNBA | @FearTheBrown | Powered by api.pbpbstats.com\nshiny.sradjoker.cc/NBA-Lineups-Trends",
+                    caption="@SravanNBA | @FearTheBrown | Powered by api.pbpstats.com\nshiny.sradjoker.cc/NBA-Lineups-Trends",
                 )
                 + theme_xkcd(base_size=16)
                 + theme(
