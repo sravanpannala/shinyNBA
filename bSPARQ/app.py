218173183f55c9c8cffa8f7d99bd0bfdbe0c3e75
Sravan Pannala <57152030+sravanpannala@users.noreply.github.com>
2025-03-29 04:48:12 -0400

    bsparq change layout to sidebar

--- bSPARQ/app.py
+++ bSPARQ/app.py
@@ -52,10 +52,14 @@ app_ui = ui.page_fluid(
             ))
         )
     ),
-    ui.row(
-        ui.column(6,ui.input_selectize("player","Player",players, selected="Kel'el Ware")),
-    ),
-    ui.output_data_frame("df_display"),
+    ui.layout_sidebar(
+        ui.sidebar(
+            ui.row(
+                ui.column(12,ui.input_selectize("player","Player",players, selected="Kel'el Ware")),
+            ),
+        ),
+        ui.output_data_frame("df_display"),
+    )
 )
 
 def server(input, output, session):
